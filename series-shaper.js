5c2c1e2dc85f1dee7d2786d669cba6b59fa7e7e3
mbradds <gmoss@ualberta.ca>
2020-12-20 14:11:52 -0700

    added transform parameter

--- series-shaper.js
+++ series-shaper.js
@@ -1,10 +1,6 @@
-const nonTidy = require("./test_data/non-tidy.json");
-const tidy = require("./test_data/tidy.json");
-
-class Series {
-  constructor(df, dataType = undefined, chartType = undefined) {
+module.exports = class Series {
+  constructor(df, chartType = undefined) {
     this._df = df;
-    this._dataType = dataType;
     this._chartType = chartType;
     this._series = undefined;
   }
@@ -28,7 +24,10 @@ class Series {
   get series() {
     return this._series;
   }
-
+  /**
+   * For best performance, data should be pre-filtered where possible. filter should only be called prior to generating a new series with "generateSeries"
+   * @param {*} filterObj Object specifying key(s) (column name) and values (column value). A subset of the original data will be returned where the column name equals the value.
+   */
   filter(filterObj) {
     for (const [key, value] of Object.entries(filterObj)) {
       if (!Array.isArray(value)) {
@@ -41,12 +40,16 @@ class Series {
     }
     return this._df;
   }
-
-  sort(by, how = "ascending") {
+  /**
+   * For best performance, data should be pre-sorted. Sort should only be called prior to generating a new series with "generateSeries"
+   * @param {*} by The column name to sort on. This column will typically be the data "index", such as a date.
+   * @param {*} how Enter either "asc" (ascending order) or "desc" (descending order). Default is "asc"
+   */
+  sort(by, how = "asc") {
     let sortedData = [];
-    if (how == "ascending") {
+    if (how == "asc") {
       sortedData = this._df.slice().sort((a, b) => b[by] - a[by]);
-    } else if (how == "descending") {
+    } else if (how == "desc") {
       sortedData = this._df.slice().sort((a, b) => a[by] - b[by]);
     }
     this._df = sortedData;
@@ -65,14 +68,6 @@ class Series {
     }
     return result;
   };
-
-  #yValues(decimals) {
-    if (decimals !== undefined) {
-      return (r, c) => (r[c] !== null ? +r[c].toFixed(decimals) : r[c]);
-    } else {
-      return (r, c) => r[c];
-    }
-  }
 
   #seriesProperties(colors) {
     if (colors !== undefined) {
@@ -100,7 +95,60 @@ class Series {
     } //TODO: add more chart types here
   }
 
-  #nonTidyOperation(xCol, yCols, colors, decimals, xName) {
+  #yValues(transform) {
+    if (
+      transform.hasOwnProperty("decimals") &&
+      !transform.hasOwnProperty("operator")
+    ) {
+      return (r, c) =>
+        r[c] !== null ? +r[c].toFixed(transform.decimals) : r[c];
+    } else if (
+      !transform.hasOwnProperty("decimals") &&
+      !transform.hasOwnProperty("operator")
+    ) {
+      return (r, c) => r[c];
+    } else if (transform.hasOwnProperty("operator")) {
+      if (transform.hasOwnProperty("decimals")) {
+        if (transform.operator == "*") {
+          return (r, c) =>
+            r[c] !== null
+              ? +(r[c] * transform.conversion).toFixed(transform.decimals)
+              : r[c];
+        } else if (transform.operator == "/") {
+          return (r, c) =>
+            r[c] !== null
+              ? +(r[c] / transform.conversion).toFixed(transform.decimals)
+              : r[c];
+        } else if (transform.conversion == "+") {
+          return (r, c) =>
+            r[c] !== null
+              ? +(r[c] + transform.conversion).toFixed(transform.decimals)
+              : r[c];
+        } else if (transform.conversion == "-") {
+          return (r, c) =>
+            r[c] !== null
+              ? +(r[c] - transform.conversion).toFixed(transform.decimals)
+              : r[c];
+        }
+      } else {
+        if (transform.operator == "*") {
+          return (r, c) =>
+            r[c] !== null ? +(r[c] * transform.conversion) : r[c];
+        } else if (transform.operator == "/") {
+          return (r, c) =>
+            r[c] !== null ? +(r[c] / transform.conversion) : r[c];
+        } else if (transform.conversion == "+") {
+          return (r, c) =>
+            r[c] !== null ? +(r[c] + transform.conversion) : r[c];
+        } else if (transform.conversion == "-") {
+          return (r, c) =>
+            r[c] !== null ? +(r[c] - transform.conversion) : r[c];
+        }
+      }
+    }
+  }
+
+  #nonTidyOperation(xCol, yCols, transform, colors, xName) {
     const seriesData = {};
     const colTotals = {};
     yCols.map((col) => {
@@ -108,7 +156,7 @@ class Series {
       colTotals[col] = 0;
     });
     xName = this.#properxName(xName);
-    const yOperator = this.#yValues(decimals);
+    const yOperator = this.#yValues(transform);
     this._df.map((row) => {
       yCols.map((col) => {
         seriesData[col].push({
@@ -129,9 +177,9 @@ class Series {
     return seriesResult;
   }
 
-  #tidyOperation(xCol, yCols, colors, decimals, valuesCol, xName) {
+  #tidyOperation(xCol, yCols, transform, colors, valuesCol, xName) {
     const variableColumn = this.#getUnique(yCols);
-    const yOperator = this.#yValues(decimals);
+    const yOperator = this.#yValues(transform);
     const seriesOperator = this.#seriesProperties(colors);
     xName = this.#properxName(xName);
     const seriesData = variableColumn.map((v) => {
@@ -148,68 +196,49 @@ class Series {
     return seriesData;
   }
 
+  #findDataType(y, valuesCol) {
+    if (Array.isArray(y) && valuesCol == undefined) {
+      this._dataType = "non-tidy";
+    } else {
+      this._dataType = "tidy";
+    }
+  }
+  /**
+   *
+   * @param {*} xCol The data column intended for the chart xAxis. Typically a date or category.
+   * @param {*} yCols Non-tidy data: The column name(s) that will be shaped into serie(s) names and chart y-values. Tidy data: The column name that contains discrete categories that will be shaped into series names and chart y-values.
+   * @param {*} transform Object defining the transformation parameters for the series y-values. Parameters include: decimals, conversion, operator
+   * @param {*} valuesCol Required for tidy data. The column name that contains numeric data. Typically called "values, value, prices, etc"
+   * @param {*} colors Optional object specifying custom colors for each series name specified in "yCols"
+   * @param {*} xName Optional. A custom key/name for the series "x" values/categories. Defaults to "x" for most chart types.
+   */
   generateSeries(
     xCol,
     yCols,
-    colors = undefined,
-    decimals = undefined,
+    transform = false,
     valuesCol = undefined, //only for tidy data with one numeric/values column.
+    colors = undefined,
     xName = undefined
   ) {
+    this.#findDataType(yCols, valuesCol);
     if (this._dataType == "non-tidy") {
       this._series = this.#nonTidyOperation(
         xCol,
         yCols,
+        transform,
         colors,
-        decimals,
         xName
       );
     } else {
       this._series = this.#tidyOperation(
         xCol,
         yCols,
+        transform,
         colors,
-        decimals,
         valuesCol,
         xName
       );
     }
     return this._series;
   }
-}
-
-// const colors = {
-//   Marine: "Red",
-//   Pipeline: "Blue",
-//   Railway: "Green",
-//   Truck: "Yellow",
-// };
-// const series = new Series(nonTidy, "non-tidy", "line");
-// let data = series.filter({ Product: "Propane", Origin: "Canada" });
-// data = series.sort("Period", "descending");
-// let hcSeries = series.generateSeries(
-//   "Period",
-//   ["Marine", "Pipeline", "Railway", "Truck"],
-//   colors,
-//   1
-// );
-// console.log(hcSeries[0]);
-
-const colors = {
-  "PADD I": "Red",
-  "PADD II": "Blue",
-  "PADD III": "Orange",
-  "PADD IV": "Green",
-  "PADD V": "Yellow",
-  Other: "Grey",
 };
-const series = new Series(tidy, "tidy", "line");
-series.sort("Year", "descending");
-let hcSeries = series.generateSeries(
-  "Year",
-  "PADD",
-  colors,
-  undefined,
-  "Value"
-);
-console.log(hcSeries[0]);
---
+++ test_data/series-tests.js
@@ -0,0 +1,49 @@
+const nonTidy = require("./non-tidy.json");
+const tidy = require("./tidy.json");
+const Series = require("../series-shaper");
+
+const transform = {
+  conversion: 6.2898,
+  operator: "*",
+  decimals: 2,
+};
+
+//non-tidy example
+// const colors = {
+//   Marine: "Red",
+//   Pipeline: "Blue",
+//   Railway: "Green",
+//   Truck: "Yellow",
+// };
+// const series = new Series(nonTidy, "line");
+// let data = series.filter({ Product: "Propane", Origin: "Canada" });
+// data = series.sort("Period", "desc");
+// let hcSeries = series.generateSeries(
+//   "Period",
+//   ["Marine", "Pipeline", "Railway", "Truck"],
+//   transform,
+//   undefined,
+//   colors
+// );
+// console.log(hcSeries[0]);
+
+//tidy example
+const colors = {
+  "PADD I": "Red",
+  "PADD II": "Blue",
+  "PADD III": "Orange",
+  "PADD IV": "Green",
+  "PADD V": "Yellow",
+  Other: "Grey",
+};
+
+const series = new Series(tidy, "line");
+series.sort("Year", "desc");
+let hcSeries = series.generateSeries(
+  "Year",
+  "PADD",
+  transform,
+  "Value",
+  colors
+);
+console.log(hcSeries[0]);
